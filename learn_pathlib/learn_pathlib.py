90b62ec027781af1e8521795afad69ca0852f49f
DennyGuo <34097154+DennyGuo33669@users.noreply.github.com>
2021-09-29 15:17:11 +0800

    学习 pathlib

--- learn_pathlib/learn_pathlib.py
+++ learn_pathlib/learn_pathlib.py
@@ -3,3 +3,10 @@
 # @Time    : 2021/9/14 16:17
 # @联系方式 :1084558196@qq.com
 # @Function:
+from pathlib import Path
+
+now_path = Path.cwd()
+home_path = Path.home()
+
+print("当前工作目录",now_path,type(now_path))
+print("home目录",home_path,type(home_path))
